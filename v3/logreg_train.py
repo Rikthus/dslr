8bd6ff66f8d31aba30fd039a07219848d52c08f0
maxperei <maxperei@student.42lyon.fr>
2024-07-28 16:54:22 +0200

    Update logistic regression almost done

--- v3/logreg_train.py
+++ v3/logreg_train.py
@@ -1,7 +1,40 @@
 import sys
 import os
 import pandas as pd
-import math
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+LEARNING_RATE = 0.01
+EPOCHS = 1000
+
+
+"""            HELPER FUNCTIONS               """
+
+
+def mean_(values: pd.Series) -> float:
+    assert len(values) != 0, "<values> should not be empty"
+    m = len(values)
+    return values.sum() / m
+
+
+def var_(values: pd.Series) -> float:
+    assert len(values) != 0, "<values> should not be empty"
+    m = len(values)
+    v_mean = mean_(values)
+    squared_mean_dist = []
+    for x in values:
+        squared_mean_dist.append((x - v_mean)**2)
+    df_squared_mean_dist = pd.Series(squared_mean_dist)
+    return df_squared_mean_dist.sum() / m
+
+
+def std_(values: pd.Series) -> float:
+    assert len(values) != 0, "<values> should not be empty"
+    return var_(values)**0.5
+
+
+"""           DATA PREPERATION           """
 
 
 def load_dataset() -> pd.DataFrame:
@@ -14,50 +47,94 @@ def load_dataset() -> pd.DataFrame:
     return df
 
 
-def min_max_normalization(value, v_min, v_max) -> float:
-    return (value - v_min) / (v_max - v_min)
+def identify_non_numeric_columns(df: pd.DataFrame) -> list[str]:
+    non_numeric_columns = []
+    for col in df.columns:
+        if not pd.api.types.is_numeric_dtype(df[col]):
+            non_numeric_columns.append(col)
+    return non_numeric_columns
 
 
-def polynomial_regression(x: float, thetas: list[float]) -> float:
-    return 0
+def standardize_data(df: pd.DataFrame) -> pd.DataFrame:
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(df)
+    df_scaled = pd.DataFrame(scaled_data, columns=df.columns)
+    return df_scaled
 
 
-def sigmoid_hypothesis_function(x: float, thetas: list[float]) -> float:
-    return 1 / (1 + math.exp(- polynomial_regression(x, thetas)))
+def format_dataset(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.drop(inplace=True, columns="Index")
+    non_numeric_columns = identify_non_numeric_columns(df)
+
+    x_df = standardize_data(df.drop(columns=non_numeric_columns, axis=1))
+    y_df = pd.get_dummies(df["Hogwarts House"])
+    for col in y_df:
+        y_df[col] = y_df[col].replace({True: 1, False: 0})
+
+    return (x_df, y_df)
 
 
-def log_loss(x, y, t0, t1) -> float:
-    if y == 1:
-        return - math.log(sigmoid_hypothesis_function(x, t0, t1))
-    else:
-        return - math.log(1 - sigmoid_hypothesis_function(x, t1, t0))
+"""             LOGISTIC REGRESSION              """
 
 
-def gradient_descent(t1, t0) -> tuple[float, float]:
-    pass
+def gradient_descent(X: np.ndarray, h: np.ndarray, Y: np.ndarray, thetas: np.ndarray) -> np.ndarray:
+    m = Y.shape[0]
+
+    gradients = X.T.dot((h - Y)) / m
+    thetas = thetas - LEARNING_RATE * gradients
+
+    return thetas
 
 
-def logistic_regression(X: pd.DataFrame, y: pd.DataFrame):
-    # normaliser la donnée
-    # calculer la fonction sigmoid hypothese en utilisant une regression lineaire multiple
-    # calculer le cout (log loss) de model actuel
-    # répéter une nombre fini de fois l'algo: Epoch
-    # utiliser la technique du one vs all pour appliquer la regression selon les 4 maisons
-    pass
+def sigmoid_function(X: np.ndarray, thetas: np.ndarray) -> np.ndarray:
+    z = X.dot(thetas)
+    val = 1 / (1 + np.exp(-z))
+    return val
+
+
+def logistic_regression(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    thetas = np.random.randn(X.shape[1], 1)
+
+    for _ in range(EPOCHS):
+        h = sigmoid_function(X, thetas)
+
+        old_thetas = thetas
+        thetas = gradient_descent(X, h, Y, thetas)
+
+        print(old_thetas)
+        print("\n")
+        print(thetas)
+
+    return thetas
+
+
+def one_vs_all(x_df: pd.DataFrame, y_df: pd.DataFrame):
+    x = x_df.to_numpy()
+    X = np.hstack((x, np.ones((x.shape[0], 1))))
+
+    for col in y_df:
+        y = y_df[col].to_numpy()
+        Y = y.reshape(y.shape[0], 1)
+
+        thetas = logistic_regression(X, Y)
+
+        print(f"WEIGHTS: {col}")
+        print(thetas)
+        print("END")
+
+        # save thetas in a file
 
 
 def main():
-    try:
-        df = load_dataset()
-        df.dropna(inplace=True)
-        df.drop_duplicates(inplace=True)
-        df.drop(inplace=True, columns="Index")
-        X = df.drop(columns=["Hogwarts House"])
-        y = df["Hogwarts House"]
-        # peut etre utiliser 'get_dummies' sur les features discretes/string pour beneficier de plus de data
-        logistic_regression(X, y)
-    except Exception as e:
-        print(f"{e.__class__.__name__}: {e.args[0]}")
+    # try:
+    pd.set_option('future.no_silent_downcasting', True)
+    df = load_dataset()
+    x_df, y_df = format_dataset(df)
+    one_vs_all(x_df, y_df)
+    # except Exception as e:
+    #     print(f"{e.__class__.__name__}: {e.args[0]}")
 
 
 if __name__ == "__main__":
